3d436930ffaba07a51ec1bbfc9c1ee1e436f28ee
unknown <ritamacharya2004@gmail.com>
2025-04-14 14:15:00 +0530

    change 1

--- src/components/Div1.jsx
+++ src/components/Div1.jsx
@@ -3,13 +3,13 @@
 function Div1() {
     return (
         <>
-            <a href="#"><div className="bg-black bg-cover bg-center relative
+            <a href="#"><div className="bg-black bg-cover bg-center  relative
       bg-[url('https://www.apple.com/v/home/bv/images/heroes/iphone-16-pro/hero_iphone16pro_avail__fnf0f9x70jiy_small.jpg')] 
       md:bg-[url('https://www.apple.com/v/home/bv/images/heroes/iphone-16-pro/hero_iphone16pro_avail__fnf0f9x70jiy_mediumtall.jpg')] 
       w-full h-[500px] md:h-[600px] object-cover bg-no-repeat text-white">
         <div className="absolute  left-[50%] translate-x-[-50%] md:top-8 bottom-[25px] w-full ">
         <div id="content1" className=" flex flex-col justify-center items-center w-full ">
-        <h1 className="text-white  font-semibold text-5xl tracking-tighter leading-[50px] ">iPhone 16 Pro</h1>
+        <h1 className="text-white  font-semibold text-5xl tracking-tighter leading-[50px] mt-10 ">iPhone 16 Pro</h1>
         <p className=" font-normal text-white  text-2xl tracking-tight  mt-[4px] leading-[40px]">Hello, Apple Intelligence.</p>
         <div className="flex justify-between items-center gap-4 mt-4" >
           <button className="text-white font-normal text-sm bg-blue-600 hover:bg-blue-500 md:text-lg md:py-2 py-1 px-2 md:w-[140px] w-[95px] rounded-full ">Learn more</button>
